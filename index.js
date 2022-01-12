0c7cd713d308330dd0aa916e1fda3ec4b45e7720
ashleysalinas <ashleysalinas98@protonmail.com>
2022-01-12 11:27:48 -0600

    starting over, created initial connection

--- index.js
+++ index.js
@@ -1,120 +1,13 @@
 const mysql = require('mysql');
-const inquirer = require('inquirer');
-const util = require('util')
-
-//schema = require('./schema.sql')
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'whippersnapper3',
-    database: 'employees_db',
-})
+    password: 'whippersnapper3' // change with .env
+});
 
 connection.connect((err) => {
     if (err) throw err;
-    console.log("Database created");
-    mainMenu()
-
-  });
- connection.query = util.promisify(connection.query)
-function mainMenu() {
-    inquirer.prompt({
-        name: 'mainMenu',
-        type: 'list',
-        message: "Please choose from the following",
-        choices: [
-            'Add department',
-            'Add role',
-            'Add employee',
-            'View departments',
-            'View roles',
-            'View employees',
-            'Update employee roles',
-            'Update employee by manager',
-            'View employees by manager',
-            'Delete department',
-            'Delete role',
-            'Delete employee',
-            'View department budget (based on salary)'
-    ]
-        }).then((answer) => {
-            switch(answer.mainMenu) {
-                case 'Add department':
-                addDepartment();
-                break;
-                case 'Add role':
-                addRole();
-                break;
-                case 'View departments':
-                viewDepartments();
-                break;
-            }
-        })
-}
-
-function addDepartment() {
-    inquirer.prompt({
-        name: 'addDeparmentName',
-        type: 'input',
-        message: 'What is the name of the department you would like to add?'
-    }).then((answer) => {
-        const query = 'INSERT INTO department SET ?';
-        connection.query(query, {
-        name: answer.addDeparmentName
-    }, (err) => {
-        if (err) throw err;
-        console.log('Your department was created successfully!');
-        mainMenu()})
-    })
-}
-
-function addRole() {
-    inquirer.prompt({
-        name: 'addRole',
-        type: 'input',
-        message: 'What is the name of the role you would like to add?'
-    },
-    {
-        name: 'addSalary',
-        type: 'input',
-        message: 'What is the salary for this role?' //add department selector
-    }).then((answer) => {
-        const query = 'INSERT INTO employee_role SET ?';
-        connection.query(query, {
-            title: answer.addRole,
-            salary: answer.addSalary
-        }, (err) => {
-            if (err) throw err;
-            console.log('Your role was created successfully!');
-            mainMenu()
-        })
-    })
-}
-
-async function viewDepartments() {
-    connection.query('SELECT * from department', (err, res) => {
-        res.forEach(({ name }) => {
-            console.log("\n");
-            console.log(name);
-        })
-    }).then(
-        confirm()
-        )
-}
-
-function confirm() {
-    inquirer.prompt({
-        name: 'confirm',
-        type: 'confirm',
-        message: 'Back to main menu?'
-    }).then((answer) => {
-        if (answer.confirm == true) {
-            mainMenu()
-        } else {
-            console.log('working')
-        }
-    })
-}
-
+    console.log('working')
+})
